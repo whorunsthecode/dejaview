54bd1ac696a21a5244b11b593559e0c0e242d6d0
rohanasrani3 <rohanasrani3@gmail.com>
2026-09-12 13:27:51 +0800

    Finished extension workflow

--- extension/content.js
+++
@@ -1,8 +0,0 @@
-// Content script. Injected into all pages.
-// Responsible for: readable-text extraction and in-page highlighting.
-// Stub: replies "not implemented" to any message.
-
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  sendResponse({ ok: false, error: "not implemented" });
-  return true;
-});
